cb10ae2b79d6c4ba73a36f95a9cfefe43f6a3739
jeppeter <jeppeter@gmail.com>
2013-04-23 18:27:33 +0800

    to add TestCaseLdr.py with class

--- src/TestCaseLdr.py
+++ src/TestCaseLdr.py
@@ -2,25 +2,27 @@
 
 import unittest
 
-def LoadSuite(st,mn,cn,fn=None):
-	rst = None
-	try:
-		m = __import__(mn)
-		cls = getattr(m,cn)
-		if fn :
-			if st:
-				st.addTest(cls(fn))
+import LocalException
+
+class LoadModuleError(LocalException.LocalException):
+	pass
+
+
+class TestCaseLoader(unittest.TestSuite):
+	def LoadCase(self,casestrstr):
+		pass 
+	def __LoadCase(self,mn,cn,fn=None):
+		rst = unittest.TestSuite()
+		try:
+			m = __import__(mn)
+			cls = getattr(m,cn)
+			if fn:
+				rst.addTest(cls(fn))
 			else:
- 				rst = unittest.TestSuite()
- 				rst.addTest(cls(fn))
- 		else:
-			tests = unittest.loader.loadTestsFromTestCase(cls)
-			if st:
-				st.addTests(tests)
-			else:
- 				rst = unittest.TestSuite()
- 				rst.addTests(tests)
-		
-	except:
-		raise LocalException.LocalException('can not load [%s].%s function %s'%(mn,cn,fn))
+				tests = unittest.loader.loadTestsFromTestCase(cls)
+				rst.addTest(tests)
+		except:
+			raise LoadModuleError('can not load [%s].%s:%s module'%(mn,cn,fn))
+		return rst
+
 
