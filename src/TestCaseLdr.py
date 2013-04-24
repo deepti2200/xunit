9683ca8f4c60ef4215fd52a1323d03b86c7fbdb4
jeppeter <jeppeter@gmail.com>
2013-04-24 17:00:11 +0800

    to move the tests into cfgtests and make the TestCaseLdr.py code complete not tested

--- src/TestCaseLdr.py
+++ src/TestCaseLdr.py
@@ -19,10 +19,39 @@ class TestCaseLoader(unittest.TestSuite):
 			if fn:
 				rst.addTest(cls(fn))
 			else:
-				tests = unittest.loader.loadTestsFromTestCase(cls)
+				tests = unittest.loader().loadTestsFromTestCase(cls)
 				rst.addTest(tests)
 		except:
 			raise LoadModuleError('can not load [%s].%s:%s module'%(mn,cn,fn))
 		return rst
 
 
+	def LoadCase(self,case):
+		'''
+			case is in the format like module.class:function
+			module is the module name
+			class is the class name in the modulef
+			function is the function name 
+		'''
+		mcname = case
+		fn = None
+		if ':' in case:
+			mcname,fn = case.split(':')
+
+		kpart = mcname
+		rst = None
+		while len(kpart) > 0:
+			r = kpart.rfind('.')
+			mn = mcname[:r]
+			cn = mcnam[r+1:]
+			try:
+				rst = self.__LoadCase(mn,cn,fn)
+			except LoadModuleError as e :
+				kpart = mn
+
+		if rst is None:
+			raise LoadModuleError('can not load %s'%(case))
+		self.addTests(rst)
+		return
+
+	
