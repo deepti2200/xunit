a63b7239cd6682eb63f73d510a4d8eb7db6b62f4
jeppeter <jeppeter@gmail.com>
2013-05-02 11:30:14 +0800

    to test logger ok

--- src/xunit/logger.py
+++ src/xunit/logger.py
@@ -4,31 +4,108 @@ import logging
 import xunit.utils.cls
 import xunit.config
 import sys
+import StringIO
+import inspect
+
+
+CRITICAL_LEVEL=1
+ERROR_LEVEL=2
+WARNING_LEVEL=3
+INFO_LEVEL=4
+DEBUG_LEVEL=5
+
+def DebugString(msg,level=1):
+	_f = inspect.stack()[level]
+	_msg = '[%s:%s] %s'%(_f[1],_f[2],msg)
+	sys.stderr.write(_msg+'\n')
+
+def DebugString2(msg,level=1):
+	_msg = '%s'%(msg)
+	sys.stderr.write(_msg+'\n')
 
 
 MAX_CASE_LEN = 70
 MAX_CASE_NAME_LEN = 55
 class BaseLogger:
 	def __init__(self,cn):
-		self.__logger = logging.getLogger(cn)
-		self.__logger.setLevel(logging.INFO)
-		formatter = logging.Formatter("[%(filename)-10s:%:%(lineno)-5s] %(message)s")
-		fh = logging.StreamHandler(None)
-		fh.setFormatter(formatter)
-		self.__logger.addHandler(fh)
+		'''
+		    init the logger ,and we do this by the string 
+		    input
+		    @cn class name of logger get
+		'''
+		self.__strio = None
+		self.__level = WARNING_LEVEL
+		self.__output = 1
+		self.__ResetStrLogger()
+		
+
+	def __ResetStrLogger(self):
+		if self.__strio :
+			self.__strio.close()
+			del self.__strio
+			self.__strio = None
+
+		# now to set the logger format
+		try:
+			# this will call error on delete function call sequence
+			self.__strio = StringIO.StringIO()
+		except:
+			pass
 		self.__caselen = 0
+		return
+
+
+	def __del__(self):
+		#DebugString2('delete %s'%(repr(self)))
+		self.Flush()
+		del self.__strio
+		self.__strio = None
+		return
+
+	def SetLevel(self,level=WARNING_LEVEL):
+		oldlevel = self.__level
+		self.__level = level
+		return oldlevel
+	def SetOutput(self,output=1):
+		oldoutput = self.__output
+		self.__output = output
+		return oldoutput
 	
 	def Info(self,msg):
-		self.__logger.info(msg)
+		if self.__level >= INFO_LEVEL:
+			_f = inspect.stack()[1]
+			_msg = '[%s:%s] %s\n'%(_f[1],_f[2],msg)
+			self.__strio.write(_msg)
 
 	def Warn(self,msg):
-		self.__logger.warning(msg)
+		if self.__level >= WARNING_LEVEL:
+			_f = inspect.stack()[1]
+			_msg = '[%s:%s] %s\n'%(_f[1],_f[2],msg)
+			self.__strio.write(_msg)
 
 	def Error(self,msg):
-		self.__logger.error(msg)
+		if self.__level >= ERROR_LEVEL:
+			_f = inspect.stack()[1]
+			_msg = '[%s:%s] %s\n'%(_f[1],_f[2],msg)
+			self.__strio.write(_msg)
 
 	def Debug(self,msg):
-		self.__logger.debug(msg)
+		if self.__level >= DEBUG_LEVEL:
+			_f = inspect.stack()[1]
+			_msg = '[%s:%s] %s\n'%(_f[1],_f[2],msg)
+			self.__strio.write(_msg)
+
+	def Flush(self):
+		value = None
+		if self.__strio :
+			try:
+				value = self.__strio.getvalue()
+				if self.__output > 0:
+					sys.stdout.write(value)
+			except TypeError:
+				value = None
+			self.__ResetStrLogger()
+		return value
 
 	def TestStart(self,msg):
 		self.__caselen = 0
@@ -148,10 +225,11 @@ class BaseLogger:
 _logger_instances = {}
 
 def singleton(cls):
-	def get_instance():
+	def get_instance(*args, **kwargs):
 		ccn = xunit.utils.cls.GetCallerClassName(2)
  		if ccn not in _logger_instances  :
- 			_logger_instances[ccn] = cls(ccn)
+ 			_logger_instances[ccn] = cls(ccn,*args, **kwargs)
+			#DebugString('caller %s %s\n'%(ccn,repr(_logger_instances[ccn])))
  		return _logger_instances[ccn]
 	return get_instance
 
@@ -160,8 +238,9 @@ def singletonbyargs(class_):
 	def getinstance(*args, **kwargs):
 		pn = args[0]
 		tn =  pn
-		if tn not in _logger_instances:
+		if tn not in _logger_instances:			
 			_logger_instances[tn] = class_(*args, **kwargs)
+			#DebugString('caller %s %s\n'%(tn,repr(_logger_instances[tn])))
 		return _logger_instances[tn]
 	return getinstance	
 	
