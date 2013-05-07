60092463698aa97925181ffd64ee33ba4482ab84
jeppeter <jeppeter@gmail.com>
2013-05-07 15:16:41 +0800

    to add one case in the test_xmllogger.py

--- tests/tstlog/test_xmllogger.py
+++ tests/tstlog/test_xmllogger.py
@@ -8,11 +8,15 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__f
 from xunit import logger
 from xunit import case
 from xunit.utils import cls
-from xunit.utils import randgen
+from xunit.utils.randgen import InitRandom,GenRandomChars,GenRandomNum
 import re
 
 
 class TestXmlLogger(case.XUnitCase):
+	@classmethod
+	def XUnitsetUpClass(cls):
+		InitRandom()
+		return
 	def XUnitsetUp(self):
 		return
 
@@ -23,4 +27,22 @@ class TestXmlLogger(case.XUnitCase):
 		cn = cls.GetClassName(self)
 		xl = logger.XmlLogger(cn)
 		xl.SetLevel(logger.DEBUG_LEVEL)
-		xl.Debug()
+		n = GenRandomNum(100,1)
+		s = GenRandomChars(n)
+		xl.Debug(s)
+		vpat = re.compile(s)
+		v = xl.Flush()
+		self.assertTrue(vpat.search(v))
+
+		xl.SetLevel(logger.WARNING_LEVEL)
+		n = GenRandomNum(100,1)
+		s = GenRandomChars(n)
+		xl.Debug(s)
+		vpat = re.compile(s)
+		v = xl.Flush()
+		self.assertTrue(v == '')
+		return
+
+	
+
+
